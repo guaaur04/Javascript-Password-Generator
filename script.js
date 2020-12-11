95936f56666e68747a61b0a148ab03f179cb1604
V Guardado <auricia.guardado@gmail.com>
2020-12-10 21:44:42 -0800

    generated variable to hold final results, as well as for loop to generate final password

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+generateBtn.addEventListener("click",function(){writePassword()})
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,73 +10,85 @@ function writePassword() {
 
 }
 
-// TODO: Create a generatePassword funtion and write the logic within this function
- 
-
-function generatePassword () {
-
-// Confirm user for special characters. Set variable. 
-
-
-// Confirm user for numbers. Set variable. 
-
-
-// Confirm user for capital letters. Set variable. 
-
-
-// Confirm user for lowercase letters. Set variable. 
-
-
-//Prompt user about password character length (at least 8 characters and no more than 128 characters). Set variable. 
-var passwordLength = prompt ("How long will your password be? (Between 8-128")
-
-//Create a conditional to verify user has chosen at least one type of character.
-
-
 // Create an array of special characters, numbers, capital letters, and lowercase letters.
 
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0,1,2,3,4,5,6,7,8,9"
-var specialCharacters = "!,@,#,$,%,?"
+var numbers = "0123456789"
+var specialCharacters = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-//Create an empty array to hold user password preferences. 
+// Create a generatePassword function.
+function generatePassword() {
+  newPassword = "";
+  passwordCharSet = "";
+
+  // Confirm user for special characters, numbers, capital, lowercase letters, and password lengths. Set variables. 
+  passwordLength = prompt("Enter password length. Select between 8-128 characters.");
+  if (Number(passwordLength < 8) || Number(passwordLength > 128) || isNaN(passwordLength)) {
+     alert("No. That isn't a number between 8 and 128! Try again.");
+  }
+  else {
+    specialCharactersInput = confirm("Would you like to include special characters?");
+
+    numbersInput = confirm("Would you like to include numbers?");
+
+    upperCaseInput = confirm("Would you like to include uppercase letters?");
+
+    lowerCaseInput = confirm("Would you like to include lowercase characters?");
+
+    //Create multiple if statements that checks if the user said yes or no to confirms. Based on response, include specific characters into empty array. 
+    //Create an empty array to hold user password preferences. 
 
 
-//Create multiple if statements that checks if the user said yes or no to confirms. Based on response, push specific characters into empty array. 
+    if (specialCharactersInput === true) {
 
+      passwordCharSet = passwordCharSet.concat(specialCharacters);
+    }
+
+    if (numbersInput === true) {
+      passwordCharSet = passwordCharSet.concat(numbers);
+    }
+
+    if (lowerCaseInput === true) {
+      passwordCharSet = passwordCharSet.concat(lowerCase);
+    }
+
+    if (upperCaseInput === true) {
+      passwordCharSet = passwordCharSet.concat(upperCase);
+    }
+  
+    //Create a conditional to verify user has chosen at least one type of character.
+
+    if (passwordCharSet === "") {
+      alert("No preferences selected. Try again.");
+    }
+
+
+    //Create a variable to hold final results
+
+    var randomNumber = Math.floor(Math.random() * passwordCharSet.length)
+
+    var randomUpper = Math.floor(Math.random() * passwordCharSet.length)
+
+    var randomLower = Math.floor(Math.random() * passwordCharSet.length)
+
+    var randomCharacter = Math.floor(Math.random() * passwordCharSet.length)
+
+    var password = "";
+
+    //I need a for loop to loop over my final array. Loop should run based on results of password length prompt.
+    //Choose randomly from our final array that holds all the characters that the user selected. Save to our final password.
+    console.log(passwordCharSet)
+    for (var i = 0; i < passwordLength; i++) {
+      var randomCharacter = passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]; 
+      password=password.concat(randomCharacter);
+      console.log("randomCharacter",randomCharacter);
+    }
+
+    console.log("password",password)
+    return password; 
+   
+
+    
+  }
 }
-
-var specialCharactersQuestion = confirm("Would you like special characters in you password?")
-var myFinalArrayofCharacters = ''
-var specialCharacters = "!,@,#,$,%,?"
-
-if (specialCharactersQuestion === true){
-  //push special characters into empty array
-  myFinalArrayofCharacters = myFinalArrayofCharacters+specialCharacters
-} 
-
-
-
-
-
-//Create a variable to hold final results. 
-
-var finalPassword;
-
-//I need a for loop to loop over my final array. Loop should run based on results of password length prompt. Choose randomly from out final array that holds all the characters that the user selected. Save to our final password.
-
-//var randomNumber Math.floor(Math.random() * length of final array)
-//inside the for loop (finalPassword = finalPassword + finalArray [randomNumber] )
-for(var i = 0; i < passwordLength; i++){
- const element = array[i];
-
-
-
-//Return the finalPassword from this function outside of the for loop at the end of this function.
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
